60316285a6417150a1680e4b73412cbb2fdbd248
benedict tan <benedict.tan@fdmgroup.com>
2023-03-06 17:22:23 +0800

    Added 3 basic model classes

---
+++ src/main/java/com/fdmgroup/timelessfinds/Model/Product.java
@@ -0,0 +1,28 @@
+package com.fdmgroup.timelessfinds.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private double sellingPrice;
+
+    @Column(nullable = false)
+    private double costPrice;
+
+    @Column(nullable = false)
+    private double profit;
+}
