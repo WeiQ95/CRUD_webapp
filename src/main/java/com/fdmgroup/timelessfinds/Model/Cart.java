b87099292bc4375462619a24a13765050ad9f0e1
weiqiang chua <weiqiang.chua@fdmgroup.com>
2023-04-11 11:20:40 +0800

    added cart class first ver

--- src/main/java/com/fdmgroup/timelessfinds/Model/Cart.java
+++ src/main/java/com/fdmgroup/timelessfinds/Model/Cart.java
@@ -1,9 +1,11 @@
 package com.fdmgroup.timelessfinds.Model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
